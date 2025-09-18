1508b27e5147eb72e9cc0542bfce3dfb17cc1b4a
lawrencejews <lawrencejews1@gmail.com>
2025-09-18 18:28:17 +0900

    enviromental configurations for the API-gateway=

--- microservices/1-gateway-service/src/server.ts
+++ microservices/1-gateway-service/src/server.ts
@@ -1,3 +1,5 @@
+import http from 'http';
+
 import { CustomError, IErrorResponse, winstonLogger } from '@lawrencejews/marketplace-shared';
 import { Application, json, Request, Response, NextFunction, urlencoded } from 'express';
 import { Logger } from 'winston';
@@ -7,9 +9,10 @@ import hpp from 'hpp';
 import helmet from 'helmet';
 import compression from 'compression';
 import { StatusCodes } from 'http-status-codes';
+import { config } from '@gateway/config';
 
 const SERVER_PORT = 4000;
-const log: Logger = winstonLogger('', 'apiGatewayServer', 'debug');
+const log: Logger = winstonLogger(`${config.ELASTIC_SEARCH_URL}`, 'apiGatewayServer', 'debug');
 
 export class GatewayServer {
   private app: Application;
@@ -21,9 +24,10 @@ export class GatewayServer {
   public start(): void {
     this.securityMiddleware(this.app);
     this.standardMiddleware(this.app);
-    this.routesMiddleware(this.app);
+    this.routesMiddleware();
     this.startElasticSearch();
     this.errorHandler(this.app);
+    this.startServer(this.app);
   }
 
   private securityMiddleware(app: Application): void {
@@ -31,9 +35,12 @@ export class GatewayServer {
     app.use(
       cookieSession({
         name: 'MarketCookieSession',
-        keys: [],
+        keys: [
+          `${config.SECRET_KEY_ONE}`,
+          `${config.SECRET_KEY_TWO}`
+        ],
         maxAge: 24 * 7 * 3600000,
-        secure: false //update with value from config
+        secure: config.NODE_ENV !== 'development' //update with value from config
         //sameSite: none
       })
     );
@@ -41,7 +48,7 @@ export class GatewayServer {
     app.use(helmet());
     app.use(cors(
       {
-        origin: '',
+        origin: config.CLIENT_URL,
         credentials: true,
         methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS']
       }
@@ -54,7 +61,7 @@ export class GatewayServer {
     app.use(urlencoded({ extended: true, limit: '200mb' }));
   }
 
-  private routesMiddleware(app: Application): void {
+  private routesMiddleware(): void {
 
   }
 
@@ -63,11 +70,13 @@ export class GatewayServer {
   }
 
   private errorHandler(app: Application): void {
+
     app.use('*', (req: Request, res: Response, next: NextFunction) => {
       const fullUrl = `${req.protocol}://${req.get('host')}${req.originalUrl}`;
       log.log('error', `${fullUrl} endpoint does not exist.`, '');
       res.status(StatusCodes.NOT_FOUND).json({ message: 'The endpoint called does not exist.' });
       next();
+
     });
 
     app.use((error: IErrorResponse, _req: Request, res: Response, next: NextFunction) => {
@@ -79,4 +88,27 @@ export class GatewayServer {
     }); 
   }
 
+  private async startServer(app: Application): Promise<void>{
+
+    try {
+      const httpServer: http.Server = new http.Server(app);
+      this.startHttpServer(httpServer);
+    } catch (error) {
+      log.log('error', 'GatewayService startServer() error method', error);
+    }
+
+  }
+
+  private async startHttpServer(httpServer: http.Server): Promise<void> {
+
+    try {
+      log.info(`GatewayServer has started with process id ${process.pid}`);
+      httpServer.listen(SERVER_PORT, () => {
+        log.info(`GatewayServer running on port ${SERVER_PORT}`);
+      });
+    } catch (error) {
+      log.log('error', 'GatewayService startServer() error method: ', error);
+    }
+
+  }
 }
