d3cbd7176f3d460ba12d60af1d21933469fd5f8d
lawrencejews <lawrencejews1@gmail.com>
2025-10-20 18:16:34 +0900

    updated the docker-compose.yml

--- microservices/3-authentication-service/src/database.ts
+++ microservices/3-authentication-service/src/database.ts
@@ -5,7 +5,7 @@ import { Sequelize } from 'sequelize';
 
 const log: Logger = winstonLogger(`${config.ELASTIC_SEARCH_URL}`, 'authDatabaseServer', 'debug');
 
-export const sequelize = new Sequelize( process.env.MYSQL_DB!,{
+export const sequelize: Sequelize = new Sequelize( process.env.MYSQL_DB!,{
 
   dialect: 'mysql',
   logging: false,
