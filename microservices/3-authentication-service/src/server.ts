887328246878efcb794ec3e17ea0d689e6e37185
lawrencejews <lawrencejews1@gmail.com>
2025-11-06 22:03:46 +0900

    debugging

--- microservices/3-authentication-service/src/server.ts
+++ microservices/3-authentication-service/src/server.ts
@@ -47,9 +47,11 @@ function securityMiddleware(app: Application): void{
   app.use((req: Request, _res: Response, next: NextFunction) => {
 
     if (req.headers.authorization) {
+
       const token = req.headers.authorization.split(' ')[1];
       const payload: IAuthPayload = verify(token, config.JWT_TOKEN!) as IAuthPayload;
       req.currentUser = payload;
+      
     }
 
     next();
